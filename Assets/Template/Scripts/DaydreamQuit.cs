f282681e3b69bcac56aeb490b47b74199a36b4d9
DarrylAugustine <78181668+DarrylAugustine@users.noreply.github.com>
2021-04-28 14:53:33 -0700

    Initial commit

---
+++ Assets/Template/Scripts/DaydreamQuit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Headjack;
+
+public class DaydreamQuit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)
+			&& (App.CurrentPlatform == App.VRPlatform.Daydream 
+				|| App.CurrentPlatform == App.VRPlatform.Cardboard)) {
+			Debug.Log("Quitting application due to Escape button pressed on Daydream/Cardboard");
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/Template/Scripts/TrackedController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Headjack
+{
+	public class TrackedController : MonoBehaviour
+	{
+		public Controller controllerType;
+		public List<Mapping> mapping;
+		private GameObject controller, optionalLeft;
+		private bool initialized;
+		public enum Controller
+		{
+			None = -1,
+			OculusCV = 0,
+			HTCVive = 1,
+			Wave = 2,
+			OculusGo = 3,
+			GearVR = 4,
+			QuestRiftS = 5
+		}
+		[System.Serializable]
+		public class Mapping
+		{
+			public Controller controllerType;
+			public GameObject gameObject, optionalLeftController;
+		}
+		public void Start()
+		{
+			controllerType = GetControllerType();
+			foreach (Mapping m in mapping)
+			{
+				if (m.controllerType == this.controllerType)
+				{
+					controller = m.gameObject;
+					optionalLeft = m.optionalLeftController;
+				}
+			}
+
+			initialized = true;
+		}
+		private void Update()
+		{
+			if (!initialized || controllerType == Controller.None) return;
+			if (optionalLeft == null)
+			{
+				controller.SetActive(VRInput.MotionControllerShow );
+			}
+			else
+			{
+				if (VRInput.remote.node == UnityEngine.XR.XRNode.LeftHand)
+				{
+					controller.SetActive(false);
+					optionalLeft.SetActive(VRInput.MotionControllerShow);
+				}
+				else
+				{
+					controller.SetActive(VRInput.MotionControllerShow);
+					optionalLeft.SetActive(false);
+				}
+			}
+			
+			transform.position = VRInput.LaserTransform.position;
+			transform.rotation = VRInput.LaserTransform.rotation;
+		}
+		public Controller GetControllerType()
+		{
+			switch (App.CurrentPlatform)
+			{
+				case App.VRPlatform.Oculus:
+					OVRPlugin.SystemHeadset headset = OVRPlugin.GetSystemHeadsetType();
+					switch (headset) {
+						case OVRPlugin.SystemHeadset.GearVR_R320:
+						case OVRPlugin.SystemHeadset.GearVR_R321:
+						case OVRPlugin.SystemHeadset.GearVR_R322:
+						case OVRPlugin.SystemHeadset.GearVR_R323:
+						case OVRPlugin.SystemHeadset.GearVR_R324:
+						case OVRPlugin.SystemHeadset.GearVR_R325:
+							return Controller.GearVR;
+						case OVRPlugin.SystemHeadset.Oculus_Go:
+							return Controller.OculusGo;
+						case OVRPlugin.SystemHeadset.Oculus_Quest:
+						case OVRPlugin.SystemHeadset.Rift_S:
+							return Controller.QuestRiftS;
+						case OVRPlugin.SystemHeadset.Rift_CV1:
+							return Controller.OculusCV;
+						default:
+							return Controller.None;
+					}
+				case App.VRPlatform.OpenVR:
+					return Controller.HTCVive;
+				case App.VRPlatform.ViveWave:
+					return Controller.Wave;
+				default:
+					return Controller.None;
+			}
+		}
+		}
+}
